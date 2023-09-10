ee2da155bf4cff81466a99455138335bb9cdfd43
BuildTools <unconfigured@null.spigotmc.org>
2023-09-10 16:41:56 -0500

    added color enums

--- src/main/java/net/nordryd/nordspigot/NordSpigot.java
+++ src/main/java/net/nordryd/nordspigot/NordSpigot.java
@@ -1,17 +1,17 @@
 package net.nordryd.nordspigot;
 
-import net.nordryd.nordspigot.text.PrefixFactory;
+import net.nordryd.nordspigot.text.ANSIColors;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NordSpigot extends JavaPlugin
 {
     @Override
     public void onEnable() {
-        getLogger().info(PrefixFactory.getPrefixedString("onEnable is called!"));
+        getLogger().info(ANSIColors.GREEN + "NordSpigot onEnable has been called!" + ANSIColors.RESET);
     }
 
     @Override
     public void onDisable() {
-        getLogger().info(PrefixFactory.getPrefixedString("onDisable is called!"));
+        getLogger().info(ANSIColors.PURPLE + "NordSpigot onDisable has been called!" + ANSIColors.RESET);
     }
 }
